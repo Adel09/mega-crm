df4d3924835a31427bc35464d17b9ff74f1cf4be
Adel09 <adelnehikhare@gmail.com>
2022-05-31 14:00:05 +0100

    commited stuff

--- megacrm/settings.py
+++ megacrm/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'media'
